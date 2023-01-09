2ade60e5be1724de4207b19ea2e7bb8a84400763
Haven Fricke <97204109+havenfricke@users.noreply.github.com>
2023-01-09 08:10:33 -0700

    initial push

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
-export default function Home() {
+import { gql } from "@apollo/client";
+import client from "client";
+
+export default function Home(props) {
+  console.log("props", props);
   return <div>Next JS &amp; WordPress course.</div>;
+}
+
+export const getStaticProps = async () => {
+  const {data} = await client.query({
+    query: gql`
+    query NewQuery {
+      nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            title
+            blocksJSON
+        }
+      }
+    }
+    `
+  })
+  return {
+   
+    props: {
+      data,
+      myexampleprop: "test", 
+      
+    }
+  }
 }
